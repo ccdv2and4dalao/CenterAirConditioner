68d183778feb2cad805c3a31147185e373bc6714
ST-Saint <st.saint.wyy@gmail.com>
2020-06-14 00:39:48 +0800

    merge

--- abstract/consensus/fan_speed.py
+++ abstract/consensus/fan_speed.py
@@ -2,15 +2,9 @@ import enum
 
 
 class FanSpeed(enum.Enum):
-<<<<<<< HEAD
-    Low = "low"
-    Medium = "medium"
-    High = "high"
-=======
     High = 'high'
     Mid = 'mid'
     Low = 'low'
     Non = 'non'
->>>>>>> 8282c2014a751be9705bd526f41790b0976c8c6f
 
     underlying_type = str
