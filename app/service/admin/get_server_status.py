fb1250fd08c91527769f868e6b44b119bb92454f
Myriad-Dreamin <camiyoru@gmail.com>
2020-06-13 10:03:19 +0800

    add admin get server status service impl

--- app/service/admin/get_server_status.py
+++ app/service/admin/get_server_status.py
@@ -1,11 +1,24 @@
+from abstract.component import MasterAirCond, ConfigurationProvider, Dispatcher
 from abstract.service.admin.get_server_status import AdminGetServerStatusService
 from proto import FailedResponse
-from proto.admin.get_server_status import AdminGetServerStatusRequest, AdminGetServerStatusResponse
+from proto.admin.get_server_status import AdminGetServerStatusRequest, AdminGetServerStatusResponse, ServerState
 
 
 class AdminGetServerStatusServiceImpl(AdminGetServerStatusService):
     def __init__(self, inj):
-        pass
+        self.master_air_cond = inj.require(MasterAirCond)  # type: MasterAirCond
+        self.cfg_provider = inj.require(ConfigurationProvider)  # type: ConfigurationProvider
+        self.dispatcher = inj.require(Dispatcher)  # type: Dispatcher
 
     def serve(self, req: AdminGetServerStatusRequest) -> AdminGetServerStatusResponse or FailedResponse:
-        pass
+        resp = AdminGetServerStatusResponse()
+        cfg = self.cfg_provider.get()
+        resp.mode, resp.current_temperature = self.master_air_cond.get_md_pair()
+        resp.update_delay = cfg.slave_default.update_delay
+        resp.metric_delay = cfg.slave_default.metric_delay
+        if self.dispatcher.is_idle():
+            resp.work_state = ServerState.Idle.value
+        else:
+            resp.work_state = ServerState.Working.value
+
+        return resp
--- proto/admin/get_server_status.py
+++ proto/admin/get_server_status.py
@@ -1,3 +1,5 @@
+import enum
+
 from proto import Request, Response
 
 
@@ -10,8 +12,19 @@ class AdminGetServerStatusRequest(Request):
         self.jwt_token = h['Authorization']
 
 
+class ServerState(enum.Enum):
+    Idle = 'idle'
+    Working = 'working'
+    Busy = 'busy'
+
+
 class AdminGetServerStatusResponse(Response):
     def __init__(self):
         super().__init__()
-        self.server_state = ''  # type: str
         self.mode = ''  # type: str
+        self.work_state = ''  # type: str
+        self.current_temperature = 0.0  # type: float
+        self.metric_delay = 100  # type: int
+        self.update_delay = 100  # type: int
+        # 使用ping方法检测服务器状态
+        # self.server_state = ''  # type: str
