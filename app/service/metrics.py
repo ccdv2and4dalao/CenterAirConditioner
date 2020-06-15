556122f69ff2038ad68a475925858455fd0688bd
Myriad-Dreamin <camiyoru@gmail.com>
2020-06-15 16:01:54 +0800

    add heart beat feature

--- app/service/metrics.py
+++ app/service/metrics.py
@@ -1,12 +1,12 @@
 from abc import abstractmethod, ABC
 from typing import Union
 
-from abstract.component import MasterAirCond
+from abstract.component import MasterAirCond, ConnectionPool
 from abstract.consensus import AirconTempConstraint
 from abstract.model import MetricModel
 from abstract.service import MetricsService
 from lib.injector import Injector
-from proto import FailedResponse, ConflictMode, InvalidTargetTemperature, MasterAirCondNotAlive
+from proto import FailedResponse, ConflictMode, InvalidTargetTemperature
 from proto.metrics import MetricsRequest, MetricsResponse
 
 
@@ -20,6 +20,7 @@ class MetricsServiceImpl(BaseMetricsServiceImpl):
     def __init__(self, inj: Injector):
         self.metric_model = inj.require(MetricModel)  # type: MetricModel
         self.master_air_cond = inj.require(MasterAirCond)  # type: MasterAirCond
+        self.connection_pool = inj.require(ConnectionPool)  # type: ConnectionPool
 
     def serve(self, req) -> Union[MetricsResponse, FailedResponse]:
         check_res = self.check_configuration(req)
@@ -30,11 +31,11 @@ class MetricsServiceImpl(BaseMetricsServiceImpl):
             return check_res
 
     def check_configuration(self, req: MetricsRequest) -> FailedResponse or None:
-        current_mode = self.master_air_cond.mode.value
-        if req.mode != current_mode:
+        current_mode = self.master_air_cond.mode
+        if req.mode != current_mode.value:
             return ConflictMode(f'conflict with current mode: want {current_mode}, got {req.mode}')
 
-        if req.temperature not in AirconTempConstraint.get_contraint(req.mode):
+        if req.temperature not in AirconTempConstraint.get_constraint(current_mode):
             return InvalidTargetTemperature(f'invalid target temperature : {req.temperature}')
         else:
             return None
@@ -44,3 +45,5 @@ class MetricsServiceImpl(BaseMetricsServiceImpl):
         #                         req.timestamp if req.timestamp else None)
         self.metric_model.insert(req.room_id, req.fan_speed,
                                  req.timestamp if req.timestamp else None)
+
+        self.connection_pool.put_heart_beat(req.room_id)
